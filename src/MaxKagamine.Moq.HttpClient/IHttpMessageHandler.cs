7fa408f200711e15cacb93b4dbd28e6ef0b00eba
Max Kagamine <max@kgmn.jp>
2018-11-19 20:19:39 -0500

    Created MockHttpMessageHandler with interface to simplify mocking protected SendAsync

---
+++ src/MaxKagamine.Moq.HttpClient/IHttpMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MaxKagamine.Moq.HttpClient
+{
+    public interface IHttpMessageHandler
+    {
+        /// <summary>
+        /// Send an HTTP request as an asynchronous operation.
+        /// </summary>
+        /// <param name="request">The HTTP request message to send.</param>
+        /// <param name="cancellationToken">The cancellation token to cancel operation.</param>
+        /// <returns>The task object representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">The request was null.</exception>
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+    }
+}
---
+++ src/MaxKagamine.Moq.HttpClient/MockHttpMessageHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net.Http;
+using Moq;
+using Moq.Language;
+using Moq.Language.Flow;
+using Moq.Protected;
+
+namespace MaxKagamine.Moq.HttpClient
+{
+    public class MockHttpMessageHandler : Mock<HttpMessageHandler>, IProtectedAsMock<HttpMessageHandler, IHttpMessageHandler>
+    {
+        public MockHttpMessageHandler() : base(MockBehavior.Strict)
+        { }
+
+        public MockHttpMessageHandler(MockBehavior mockBehavior) : base(mockBehavior)
+        { }
+
+        #region IProtectedAsMock
+        private IProtectedAsMock<HttpMessageHandler, IHttpMessageHandler> Protected => this.Protected().As<IHttpMessageHandler>();
+
+        public ISetup<HttpMessageHandler> Setup(Expression<Action<IHttpMessageHandler>> expression) => Protected.Setup(expression);
+        public ISetup<HttpMessageHandler, TResult> Setup<TResult>(Expression<Func<IHttpMessageHandler, TResult>> expression) => Protected.Setup(expression);
+        public ISetupGetter<HttpMessageHandler, TProperty> SetupGet<TProperty>(Expression<Func<IHttpMessageHandler, TProperty>> expression) => Protected.SetupGet(expression);
+        public Mock<HttpMessageHandler> SetupProperty<TProperty>(Expression<Func<IHttpMessageHandler, TProperty>> expression, TProperty initialValue = default(TProperty)) => Protected.SetupProperty(expression, initialValue);
+        public ISetupSequentialResult<TResult> SetupSequence<TResult>(Expression<Func<IHttpMessageHandler, TResult>> expression) => Protected.SetupSequence(expression);
+        public ISetupSequentialAction SetupSequence(Expression<Action<IHttpMessageHandler>> expression) => Protected.SetupSequence(expression);
+        public void Verify(Expression<Action<IHttpMessageHandler>> expression, Times? times = null, string failMessage = null) => Protected.Verify(expression, times, failMessage);
+        public void Verify<TResult>(Expression<Func<IHttpMessageHandler, TResult>> expression, Times? times = null, string failMessage = null) => Protected.Verify(expression, times, failMessage);
+        public void VerifyGet<TProperty>(Expression<Func<IHttpMessageHandler, TProperty>> expression, Times? times = null, string failMessage = null) => Protected.VerifyGet(expression, times, failMessage);
+        #endregion
+    }
+}
