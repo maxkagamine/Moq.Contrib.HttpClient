a232492f034bfe8597398935ac50d4df1a1c66b8
Max Kagamine <max@kgmn.jp>
2018-11-23 03:19:50 -0500

    Added same ReturnsResponse helpers to SetupSequence

--- src/MaxKagamine.Moq.HttpClient/ReturnsResponseExtensions.cs
+++ src/MaxKagamine.Moq.HttpClient/ReturnsResponseExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -7,12 +6,29 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using Moq.Language;
 using Moq.Language.Flow;
 
 namespace MaxKagamine.Moq.HttpClient
 {
     public static partial class MockHttpMessageHandlerExtensions
     {
+        private static HttpResponseMessage CreateResponse(HttpStatusCode statusCode, Action<HttpResponseMessage> configure, HttpContent content = null, string mediaType = null)
+        {
+            var response = new HttpResponseMessage(statusCode);
+
+            if (content != null)
+            {
+                if (mediaType != null)
+                    content.Headers.ContentType = new MediaTypeHeaderValue(mediaType);
+
+                response.Content = content;
+            }
+
+            configure?.Invoke(response);
+            return response;
+        }
+
         /// <summary>
         /// Specifies the response to return.
         /// </summary>
@@ -22,12 +38,18 @@ namespace MaxKagamine.Moq.HttpClient
         public static IReturnsResult<HttpMessageHandler> ReturnsResponse(
             this ISetup<HttpMessageHandler, Task<HttpResponseMessage>> setup,
             HttpStatusCode statusCode, Action<HttpResponseMessage> configure = null)
-        {
-            var response = new HttpResponseMessage(statusCode);
+            => setup.ReturnsAsync(CreateResponse(statusCode, configure));
 
-            configure?.Invoke(response);
-            return setup.ReturnsAsync(response);
-        }
+        /// <summary>
+        /// Specifies the response to return in sequence.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
+            HttpStatusCode statusCode, Action<HttpResponseMessage> configure = null)
+            => setup.ReturnsAsync(CreateResponse(statusCode, configure));
 
         /// <summary>
         /// Specifies the response to return.
@@ -43,13 +65,24 @@ namespace MaxKagamine.Moq.HttpClient
             if (content == null)
                 throw new ArgumentNullException(nameof(content));
 
-            var response = new HttpResponseMessage(statusCode)
-            {
-                Content = content
-            };
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, content));
+        }
 
-            configure?.Invoke(response);
-            return setup.ReturnsAsync(response);
+        /// <summary>
+        /// Specifies the response to return in sequence.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="content">The response content.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
+            HttpStatusCode statusCode, HttpContent content, Action<HttpResponseMessage> configure = null)
+        {
+            if (content == null)
+                throw new ArgumentNullException(nameof(content));
+
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, content));
         }
 
         /// <summary>
@@ -68,7 +101,26 @@ namespace MaxKagamine.Moq.HttpClient
             if (content == null)
                 throw new ArgumentNullException(nameof(content));
 
-            return setup.ReturnsResponse(statusCode, new StringContent(content, encoding, mediaType), configure);
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, new StringContent(content, encoding, mediaType)));
+        }
+
+        /// <summary>
+        /// Specifies the response to return in sequence, as <see cref="StringContent" />.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="content">The response body.</param>
+        /// <param name="mediaType">The media type. Defaults to text/plain.</param>
+        /// <param name="encoding">The character encoding. Defaults to <see cref="Encoding.UTF8" />.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
+            HttpStatusCode statusCode, string content, string mediaType = null, Encoding encoding = null, Action<HttpResponseMessage> configure = null)
+        {
+            if (content == null)
+                throw new ArgumentNullException(nameof(content));
+
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, new StringContent(content, encoding, mediaType)));
         }
 
         /// <summary>
@@ -81,6 +133,19 @@ namespace MaxKagamine.Moq.HttpClient
         /// <param name="configure">An action to further configure the response such as setting headers.</param>
         public static IReturnsResult<HttpMessageHandler> ReturnsResponse(
             this ISetup<HttpMessageHandler, Task<HttpResponseMessage>> setup,
+            string content, string mediaType = null, Encoding encoding = null, Action<HttpResponseMessage> configure = null)
+            => setup.ReturnsResponse(HttpStatusCode.OK, content, mediaType, encoding, configure);
+
+        /// <summary>
+        /// Specifies the response to return in sequence, as <see cref="StringContent" /> with <see cref="HttpStatusCode.OK" />.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="content">The response body.</param>
+        /// <param name="mediaType">The media type. Defaults to text/plain.</param>
+        /// <param name="encoding">The character encoding. Defaults to <see cref="Encoding.UTF8" />.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
             string content, string mediaType = null, Encoding encoding = null, Action<HttpResponseMessage> configure = null)
             => setup.ReturnsResponse(HttpStatusCode.OK, content, mediaType, encoding, configure);
 
@@ -99,11 +164,25 @@ namespace MaxKagamine.Moq.HttpClient
             if (content == null)
                 throw new ArgumentNullException(nameof(content));
 
-            var httpContent = new ByteArrayContent(content);
-            if (mediaType != null)
-                httpContent.Headers.ContentType = new MediaTypeHeaderValue(mediaType);
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, new ByteArrayContent(content), mediaType));
+        }
 
-            return setup.ReturnsResponse(statusCode, httpContent, configure);
+        /// <summary>
+        /// Specifies the response to return in sequence, as <see cref="ByteArrayContent" />.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="content">The response body.</param>
+        /// <param name="mediaType">The media type.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
+            HttpStatusCode statusCode, byte[] content, string mediaType = null, Action<HttpResponseMessage> configure = null)
+        {
+            if (content == null)
+                throw new ArgumentNullException(nameof(content));
+
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, new ByteArrayContent(content), mediaType));
         }
 
         /// <summary>
@@ -115,6 +194,18 @@ namespace MaxKagamine.Moq.HttpClient
         /// <param name="configure">An action to further configure the response such as setting headers.</param>
         public static IReturnsResult<HttpMessageHandler> ReturnsResponse(
             this ISetup<HttpMessageHandler, Task<HttpResponseMessage>> setup,
+            byte[] content, string mediaType = null, Action<HttpResponseMessage> configure = null)
+            => setup.ReturnsResponse(HttpStatusCode.OK, content, mediaType, configure);
+
+        /// <summary>
+        /// Specifies the response to return in sequence, as <see cref="ByteArrayContent" /> with <see cref="HttpStatusCode.OK" />.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="content">The response body.</param>
+        /// <param name="mediaType">The media type.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
             byte[] content, string mediaType = null, Action<HttpResponseMessage> configure = null)
             => setup.ReturnsResponse(HttpStatusCode.OK, content, mediaType, configure);
 
@@ -133,11 +224,25 @@ namespace MaxKagamine.Moq.HttpClient
             if (content == null)
                 throw new ArgumentNullException(nameof(content));
 
-            var httpContent = new StreamContent(content);
-            if (mediaType != null)
-                httpContent.Headers.ContentType = new MediaTypeHeaderValue(mediaType);
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, new StreamContent(content), mediaType));
+        }
 
-            return setup.ReturnsResponse(statusCode, httpContent, configure);
+        /// <summary>
+        /// Specifies the response to return in sequence, as <see cref="StreamContent" />.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="content">The response body.</param>
+        /// <param name="mediaType">The media type.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
+            HttpStatusCode statusCode, Stream content, string mediaType = null, Action<HttpResponseMessage> configure = null)
+        {
+            if (content == null)
+                throw new ArgumentNullException(nameof(content));
+
+            return setup.ReturnsAsync(CreateResponse(statusCode, configure, new StreamContent(content), mediaType));
         }
 
         /// <summary>
@@ -149,6 +254,18 @@ namespace MaxKagamine.Moq.HttpClient
         /// <param name="configure">An action to further configure the response such as setting headers.</param>
         public static IReturnsResult<HttpMessageHandler> ReturnsResponse(
             this ISetup<HttpMessageHandler, Task<HttpResponseMessage>> setup,
+            Stream content, string mediaType = null, Action<HttpResponseMessage> configure = null)
+            => setup.ReturnsResponse(HttpStatusCode.OK, content, mediaType, configure);
+
+        /// <summary>
+        /// Specifies the response to return in sequence, as <see cref="StreamContent" /> with <see cref="HttpStatusCode.OK" />.
+        /// </summary>
+        /// <param name="setup">The setup.</param>
+        /// <param name="content">The response body.</param>
+        /// <param name="mediaType">The media type.</param>
+        /// <param name="configure">An action to further configure the response such as setting headers.</param>
+        public static ISetupSequentialResult<Task<HttpResponseMessage>> ReturnsResponse(
+            this ISetupSequentialResult<Task<HttpResponseMessage>> setup,
             Stream content, string mediaType = null, Action<HttpResponseMessage> configure = null)
             => setup.ReturnsResponse(HttpStatusCode.OK, content, mediaType, configure);
     }
